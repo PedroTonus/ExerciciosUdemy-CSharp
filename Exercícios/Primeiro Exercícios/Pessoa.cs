7fe3597aa5dc76c928371be8cc4db3d15d3774d7
XunesTonus <66918391+XunesTonus@users.noreply.github.com>
2021-02-16 13:25:25 -0300

    Add files via upload

---
+++ Exercícios/Primeiro Exercícios/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace ExercícioClasses_1
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ Exercícios/Primeiro Exercícios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExercícioClasses_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa X = new Pessoa();
+            Pessoa Y = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            X.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            X.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Nome: ");
+            Y.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            Y.Idade = int.Parse(Console.ReadLine());
+            
+            if(Y.Idade > X.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + Y.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + X.Nome);
+            }
+
+        }
+    }
+}
